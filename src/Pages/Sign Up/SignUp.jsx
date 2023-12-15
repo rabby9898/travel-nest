2d961e0512b5922a9c2048b44934d8a4fdee5218
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-15 17:13:50 +0600

    sign up page design added

--- src/Pages/Sign Up/SignUp.jsx
+++ src/Pages/Sign Up/SignUp.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
         <div className="mb-8 text-center">
-          <h1 className="my-3 text-4xl font-bold">Log In</h1>
+          <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
           <p className="text-sm text-gray-400">
-            Login in to access your account
+            Welcome to Travel Nest Booking
           </p>
         </div>
         <form
@@ -17,6 +17,31 @@ const Login = () => {
           className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
           <div className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter Your Name Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#004aad] bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
+              />
+            </div>
+            <div>
+              <label htmlFor="image" className="block mb-2 text-sm">
+                Select Image:
+              </label>
+              <input
+                required
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
+              />
+            </div>
             <div>
               <label htmlFor="email" className="block mb-2 text-sm">
                 Email address
@@ -40,7 +65,7 @@ const Login = () => {
               <input
                 type="password"
                 name="password"
-                autoComplete="current-password"
+                autoComplete="new-password"
                 id="password"
                 required
                 placeholder="*******"
@@ -58,15 +83,10 @@ const Login = () => {
             </button>
           </div>
         </form>
-        <div className="space-y-1">
-          <button className="text-xs hover:underline hover:text-blue-800 text-gray-400">
-            Forgot password?
-          </button>
-        </div>
         <div className="flex items-center pt-4 space-x-1">
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
           <p className="px-3 text-sm dark:text-gray-400">
-            Login with social accounts
+            Signup with social accounts
           </p>
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
@@ -76,12 +96,12 @@ const Login = () => {
           <p>Continue with Google</p>
         </div>
         <p className="px-6 text-sm text-center text-gray-400">
-          Don&apos;t have an account yet?{" "}
+          Already have an account?{" "}
           <Link
-            to="/signup"
+            to="/login"
             className="hover:underline hover:text-blue-800 text-gray-600"
           >
-            Sign up
+            Login
           </Link>
           .
         </p>
@@ -90,4 +110,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/Sign Up/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "signUp",
+        element: <SignUp></SignUp>,
       },
     ],
   },
