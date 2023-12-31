926295522fc5742696e43aba3c22f32f6812a592
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-31 21:24:05 +0600

    bookings api added

--- src/Api/Bookings.jsx
+++ src/Api/Bookings.jsx
@@ -1,6 +1,17 @@
 import axiosSecure from "./AxiosSecure";
 
 export const createPaymentIntent = async (price) => {
-  const { data } = await axiosSecure("/create-payment-intent", price);
+  const { data } = await axiosSecure.post("/create-payment-intent", price);
+  return data;
+};
+// save booking info in db
+export const saveBookingInfo = async (paymentInfo) => {
+  const { data } = await axiosSecure.post("/bookings", paymentInfo);
+  return data;
+};
+
+// update room status after booking in db
+export const updateStatus = async (id, status) => {
+  const { data } = await axiosSecure.patch(`/rooms/status/${id}`, { status });
   return data;
 };
