8972bebe807ba465177a08e7a8034bea2711870a
rabby9898 <mdfajlerabby00@gmail.com>
2024-01-02 15:41:53 +0600

    update user modal

--- src/Pages/Dashboard/Admin/ManageUsers/UsersDataRow.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers/UsersDataRow.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import UpdateUserModal from "../../../../Components/Dashboard/Modal/UpdateUserModal";
+
 const UsersDataRow = ({ user }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalHandler = async (selected) => {
+    console.log(selected);
+  };
   return (
     <tr>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -22,7 +29,10 @@ const UsersDataRow = ({ user }) => {
       </td>
 
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <span className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+        <span
+          onClick={() => setIsOpen(true)}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
           <span
             aria-hidden="true"
             className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
@@ -30,6 +40,12 @@ const UsersDataRow = ({ user }) => {
           <span className="relative">Update Role</span>
         </span>
         {/* Modal */}
+        <UpdateUserModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          modalHandler={modalHandler}
+          user={user}
+        />
       </td>
     </tr>
   );
